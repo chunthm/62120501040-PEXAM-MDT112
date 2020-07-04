50bcb67aa5734a7821ebde2369da7d504d694541
Chanthima Pinkaew <61321298+chunthm@users.noreply.github.com>
2020-07-04 15:26:42 +0700

    8 complete

---
+++ 8.ino
@@ -0,0 +1,72 @@
+#include <Servo.h>
+Servo myservo;
+const int buzzer = 8;
+int count = 0;
+bool CheckStage = true;
+bool Degree = true;
+int DegreeOfServo = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+  delay(650);
+  tone(buzzer, 600);
+  delay(400);
+  noTone(buzzer);
+  myservo.attach(6);
+  myservo.write(0);
+  pinMode(2, INPUT_PULLUP);
+}
+
+void loop()
+{
+  if (Degree == true)
+  {
+    if (DegreeOfServo <= 180)
+    {
+      if (digitalRead(2) == 0)
+      {
+        myservo.write(DegreeOfServo);
+        DegreeOfServo += 10;
+        if (CheckStage == true)
+        {
+          count++;
+          CheckStage = false;
+        }
+        else
+        {
+          CheckStage = true;
+        }
+      }
+    }
+    else
+    {
+      Degree = false;
+    }
+  }
+  else {
+    if (DegreeOfServo >= 0)
+    {
+      if (digitalRead(2) == 0)
+      {
+        myservo.write(DegreeOfServo);
+        DegreeOfServo -= 10;
+        if (CheckStage == true)
+        {
+          count--;
+          CheckStage = false;
+        }
+        else
+        {
+          CheckStage = true;
+        }
+      }
+    }
+    else
+    {
+      Degree = true;
+    }
+  }
+  delay(1000);
+}
