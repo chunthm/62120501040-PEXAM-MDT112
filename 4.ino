c363693574ded3581cb8354dc77d4d5e30d852f2
Chanthima Pinkaew <61321298+chunthm@users.noreply.github.com>
2020-07-04 15:57:20 +0700

    4 completes

--- 4.ino
+++ 4.ino
@@ -5,7 +5,7 @@ void setup() {
   Serial.begin(9600);
   myservo.attach(6);
   myservo.write(180);
-  delay(1000);
+  delay(2000);
   myservo.write(0);
   delay(900);
   Serial.print("Servo : 0 degrees");
